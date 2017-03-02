c4f30687ffcc06e00da9a359f7570995ff7f6230
vickyseven <mariavittoriapatti@gmail.com>
2017-03-02 15:49:45 +0000

    Hoopla Stall & Coconut Shy adjusted
    Set up Coconut Shy and changed Hoopla Stall a little bit.
    Added the sound mechanics for those 2 minigames

---
+++ Source/fairgroundBase/BallStaticMeshComponent.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Components/StaticMeshComponent.h"
+#include "BallStaticMeshComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FAIRGROUNDBASE_API UBallStaticMeshComponent : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
